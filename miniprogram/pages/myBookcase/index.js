39d57f8d2b433e38b44b9adaf094dffb9cbe1da8
poyiding <15869507101@163.com>
2019-03-17 18:38:55 +0800

    add: bookCase pages

--- miniprogram/pages/myBookcase/index.js
+++ miniprogram/pages/myBookcase/index.js
@@ -1,18 +1,30 @@
-// miniprogram/pages/myBookcase/index.js
+const app = getApp();
+const db = wx.cloud.database();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    bookList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: '我的书柜'
+    });
+    db.collection('bookCollection').where({
+      _openid: 'o2aK45d_ryZH8lsH3I-QHGjNU824',
+    })
+    // .skip(5) // 跳过结果集中的前 10 条，从第 11 条开始返回
+    // .limit(5)
+    .get({
+      success: res => {
+        if (res.data && res.data.length > 0) {
+          this.setData({
+            bookList: res.data
+          });
+        }
+      }
+    });
   },
-
+  onReachBottom: function() {
+    // todo paganation
+  },
 })
