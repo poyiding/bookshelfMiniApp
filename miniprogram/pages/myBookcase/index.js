81bc37023d3d714f2bdc7e406444741e3c8f04ce
poyiding <15869507101@163.com>
2019-03-20 18:49:58 +0800

    add: 我的书柜分页和暂无数据template

--- miniprogram/pages/myBookcase/index.js
+++ miniprogram/pages/myBookcase/index.js
@@ -3,7 +3,11 @@ const db = wx.cloud.database();
 
 Page({
   data: {
-    bookList: []
+    bookList: [],
+    limit: 5, // 每页数据条数
+    currentPage: 1, // 当前页
+    total: 0, // 总条数
+    pages: 0, // 总页数
   },
   onLoad: function () {
     wx.setNavigationBarTitle({
@@ -11,20 +15,52 @@ Page({
     });
     db.collection('bookCollection').where({
       _openid: 'o2aK45d_ryZH8lsH3I-QHGjNU824',
-    })
-    // .skip(5) // 跳过结果集中的前 10 条，从第 11 条开始返回
-    // .limit(5)
-    .get({
+    }).count({
       success: res => {
-        if (res.data && res.data.length > 0) {
-          this.setData({
-            bookList: res.data
-          });
+        const totalSize = res.total || 0;
+        if (totalSize > 0) {
+         this.setData({
+           total: totalSize,
+           pages: Math.ceil(totalSize / 5)
+         })
         }
       }
+    })
+    this.queryList();
+  },
+  queryList: function() {
+    wx.showLoading({
+      title: '加载中',
     });
+    const { limit, currentPage, pages, bookList } = this.data;
+    const skipNum = (currentPage -1) * limit; // 例如查第2页数据：就要跳过(2 -1 ) * 5 条，从第6条开始查询
+    db.collection('bookCollection').where({
+      _openid: 'o2aK45d_ryZH8lsH3I-QHGjNU824',
+    })
+      .skip(currentPage - 1)
+      .limit(limit)
+      .get({
+        success: res => {
+          wx.hideLoading();
+          if (res.data && res.data.length > 0) {
+            const connectData = bookList.concat(res.data);
+            this.setData({
+              bookList: connectData
+            });
+          }
+        }
+      });
   },
   onReachBottom: function() {
-    // todo paganation
+    const { currentPage, pages } = this.data;
+    if (currentPage !== pages) {
+      console.log(pages, 'get pages');
+      const current = currentPage + 1;
+      this.setData({
+        currentPage: current
+      })
+      this.queryList();
+    }
+
   },
 })
